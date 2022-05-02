b72c5063edc5d1d28ea0c490163e0e10de95754e
Michael Alao <contactleomax@gmail.com>
2022-05-02 09:53:50 +0100

    Heroku

--- config/database.ts
+++ config/database.ts
@@ -5,10 +5,10 @@
  * file.
  */
 
-import Env from '@ioc:Adonis/Core/Env'
-import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+// import Env from '@ioc:Adonis/Core/Env'
+// import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
-const databaseConfig: DatabaseConfig = {
+// const databaseConfig: DatabaseConfig = {
   /*
   |--------------------------------------------------------------------------
   | Connection
@@ -19,9 +19,9 @@ const databaseConfig: DatabaseConfig = {
   | file.
   |
   */
-  connection: Env.get('DB_CONNECTION'),
+  // connection: Env.get('DB_CONNECTION'),
 
-  connections: {
+  // connections: {
     /*
     |--------------------------------------------------------------------------
     | PostgreSQL config
@@ -33,23 +33,63 @@ const databaseConfig: DatabaseConfig = {
     | npm i pg
     |
     */
+//     pg: {
+//       client: 'pg',
+//       connection: {
+//         host: Env.get('PG_HOST'),
+//         port: Env.get('PG_PORT'),
+//         user: Env.get('PG_USER'),
+//         password: Env.get('PG_PASSWORD', ''),
+//         database: Env.get('PG_DB_NAME'),
+//       },
+//       migrations: {
+//         naturalSort: true,
+//       },
+//       healthCheck: true,
+//       debug: false,
+//     },
+
+//   }
+// }
+
+// export default databaseConfig
+
+import Env from '@ioc:Adonis/Core/Env'
+import { OrmConfig } from '@ioc:Adonis/Lucid/Orm'
+import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+import Application from '@ioc:Adonis/Core/Application'
+
+const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
+  connection: Env.get('DB_CONNECTION'),
+
+  connections: {
     pg: {
       client: 'pg',
-      connection: {
-        host: Env.get('PG_HOST'),
-        port: Env.get('PG_PORT'),
-        user: Env.get('PG_USER'),
-        password: Env.get('PG_PASSWORD', ''),
-        database: Env.get('PG_DB_NAME'),
-      },
-      migrations: {
-        naturalSort: true,
-      },
-      healthCheck: true,
-      debug: false,
+      connection: Application.inProduction
+        ? Env.get('DATABASE_URL') + '?ssl=no-verify'
+        : {
+            host: Env.get('PG_HOST'),
+            port: Env.get('PG_PORT'),
+            user: Env.get('PG_USER'),
+            password: Env.get('PG_PASSWORD', ''),
+            database: Env.get('PG_DB_NAME'),
+          },
+
+      healthCheck: Application.inDev,
+      debug: Application.inDev,
     },
 
-  }
+    /*
+I usually use this custom connection to run database migrations on remote database from my local machine. For instance,
+node ace migration:run --connection=custom, this will run the migration against you remote database. In order to use this connection, you must set DATABASE_URL in .env file, you can get the value from your heroku dashboard, or by running this command on your terminal: heroku config:get DATABASE_URL --app=your_app_name
+*/
+    custom: {
+      client: 'pg',
+      connection: Env.get('DATABASE_URL') + '?ssl=no-verify',
+    },
+  },
+
+  orm: {},
 }
 
 export default databaseConfig
