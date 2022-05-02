3a791e5bb3eab487473dca6eefe55e2096d72f47
Michael Alao <contactleomax@gmail.com>
2022-05-02 15:18:52 +0100

    --- Auto-Git Commit ---

--- app/Helpers/CalculateRate.ts
+++ app/Helpers/CalculateRate.ts
@@ -4,31 +4,67 @@ const generateRate = (amount, period) => {
   return new Promise((resolve, reject) => {
     if (!amount || !period || amount <= 0) reject(new Error('Incomplete parameters'))
     let rate
-    console.log(parseInt(period))
-    console.log(parseInt(period) > 731)
-    if (period > 1000)
+    if (parseInt(period) > 89 && < 180) {
+      period = '3 months'
+    } else if (parseInt(period) === 180 || parseInt(period) < 270) {
+      period = '6 months'
+    } else if (parseInt(period) === 270 || parseInt(period) < 360) {
+      period = '9 months'
+    } else if (parseInt(period) === 360 || parseInt(period) < 450) {
+      period = '12 months'
+    } else if (parseInt(period) === 450 || parseInt(period) < 540) {
+      period = '1 year and 3 months'
+    } else if (parseInt(period) === 540 || parseInt(period) < 630) {
+      period = '1 year and 6 months'
+    } else if (parseInt(period) === 630 || parseInt(period) < 720) {
+      period = '1 year and 9 months'
+    } else if (parseInt(period) === 720) {
+      period = '2 years'
+    }
+
+
     switch (period) {
-      case '180':
+      case '3 months':
         rate = 0.06
-        console.log('RATE:', rate)
+        console.log(`RATE for ${period} is:`, rate)
         break
-      case '300':
+      case '6 months':
         rate = 0.07
-        console.log('RATE:', rate)
+        console.log(`RATE for ${period} is:`, rate)
         break
-      case 'above 1000':
-        rate = 0.1
-        console.log('RATE:', rate)
+      case '9 months':
+        rate = 0.08
+        console.log(`RATE for ${period} is:`, rate)
+        break
+      case '12 months':
+        rate = 0.09
+        console.log(`RATE for ${period} is:`, rate)
+        break
+      case '1 year and 3 months':
+        rate = 0.10
+        console.log(`RATE for ${period} is:`, rate)
+        break
+      case '1 year and 6 months':
+        rate = 0.11
+        console.log(`RATE for ${period} is:`, rate)
+        break
+      case '1 year and 9 months':
+        rate = 0.12
+        console.log(`RATE for ${period} is:`, rate)
+        break
+      case '2 years':
+        rate = 0.13
+        console.log(`RATE for ${period} is:`, rate)
         break
       default:
         rate = 0.05
-        console.log('RATE:', rate)
+        console.log(`RATE for ${period} days is:`, rate)
         break
     }
     return resolve(rate)
   })
 }
 
-generateRate(10, '756')
+generateRate(10, '89')
 // @ts-ignore
 // export generateRate
