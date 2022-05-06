0cde2d9d7caba1386b9d88849c23950a712f8423
Michael Alao <contactleomax@gmail.com>
2022-05-06 16:00:20 +0100

    --- Auto-Git Commit ---

--- app/Models/Rate.ts
+++ app/Models/Rate.ts
@@ -1,9 +1,44 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
+type RollOverType = '100' | '101' | '102' | '103' | '104' | '105' | '106' | '107'
+
 export default class Rate extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public productName: string
+
+  @column()
+  public amount: number
+
+  @column()
+  public duration: string
+
+  @column()
+  public rolloverCode: RollOverType
+
+  @column()
+  public investmentType: 'fixed' | 'debenture'
+
+  @column()
+  public tagName: string
+
+  @column()
+  public currencyCode: string
+
+  @column()
+  public walletHolderDetails: JSON
+
+  @column()
+  public long: number
+
+  @column()
+  public lat: number
+
+  @column()
+  public interestRate: number
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
