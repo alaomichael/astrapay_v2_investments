4bd757d21cb55367857fcc1d4a3b4f096f90ff6f
Michael Alao <contactleomax@gmail.com>
2022-05-10 16:49:44 +0100

    --- Auto-Git Commit ---

--- app/Controllers/Http/InvestmentsController.ts
+++ app/Controllers/Http/InvestmentsController.ts
@@ -247,7 +247,7 @@ export default class InvestmentsController {
 
     // @ts-ignore
     Event.emit('new:investment', { id: investment.id, email: investment.walletHolderDetails.email })
-    return response.status(201).json({status:'ok', data: investment})
+    return response.status(201).json({ status: 'ok', data: investment })
   }
 
   // public async store2({ request }: HttpContextContract) {
@@ -432,51 +432,60 @@ export default class InvestmentsController {
       // const investment = await Investment.query().where('user_id', id).where('id', params.id).delete()
       // let investment = await Investment.query().where('user_id', id).where('id', params.id)
       let investment = await Investment.query().where('id', investmentId)
-      console.log('investment search data :', investment[0].$original)
-      // @ts-ignore
-      let isDueForPayout = await dueForPayout(investment[0].createdAt, investment[0].duration)
-      console.log('Is due for payout status :', isDueForPayout)
+      console.log('Investment Info, line 435: ', investment)
+      if (investment.length > 0) {
+        console.log('investment search data :', investment[0].$original)
+        // @ts-ignore
+        let isDueForPayout = await dueForPayout(investment[0].createdAt, investment[0].duration)
+        console.log('Is due for payout status :', isDueForPayout)
 
-      if (isDueForPayout) {
-        let payload = investment[0].$original
-        // Date payout was effected
-        payload.datePayoutWasDone = new Date().toISOString()
-        console.log('Payout investment data 1:', payload)
-        const payout = await Payout.create(payload)
-        payout.status = 'payout'
-        await payout.save()
-        console.log('Payout investment data 2:', payout)
-        // investment = await Investment.query().where('id', params.id).where('user_id', id).delete()
-        investment = await Investment.query().where('id', investmentId)
-        investment[0].status = 'payout'
-        // Date payout was effected
-        // @ts-ignore
-        investment[0].datePayoutWasDone = new Date().toISOString()
-        investment[0].save()
-        console.log('Investment data after payout 2:', investment)
-        return response.status(200).json({
-          status: 'ok',
-          data: investment,
-        })
+        if (isDueForPayout) {
+          let payload = investment[0].$original
+          // Date payout was effected
+          payload.datePayoutWasDone = new Date().toISOString()
+          console.log('Payout investment data 1:', payload)
+          const payout = await Payout.create(payload)
+          payout.status = 'payout'
+          await payout.save()
+          console.log('Payout investment data 2:', payout)
+          // investment = await Investment.query().where('id', params.id).where('user_id', id).delete()
+          investment = await Investment.query().where('id', investmentId)
+          investment[0].status = 'payout'
+          // Date payout was effected
+          // @ts-ignore
+          investment[0].datePayoutWasDone = new Date().toISOString()
+          investment[0].save()
+          console.log('Investment data after payout 2:', investment)
+          return response.status(200).json({
+            status: 'ok',
+            data: investment,
+          })
+        } else {
+          let payload = investment[0].$original
+          // Date payout was effected due to termination
+          payload.datePayoutWasDone = new Date().toISOString()
+          console.log('Payout investment data 1:', payload)
+          const payout = await Payout.create(payload)
+          payout.status = 'terminated'
+          await payout.save()
+          console.log('Terminated Payout investment data 1:', payout)
+          // investment = await Investment.query().where('id', params.id).where('user_id', id).delete()
+          investment = await Investment.query().where('id', investmentId)
+          investment[0].status = 'terminated'
+          // @ts-ignore
+          investment[0].datePayoutWasDone = new Date().toISOString()
+          investment[0].save()
+          console.log('Terminated Payout investment data 2:', investment)
+          return response.status(200).json({
+            status: 'ok',
+            data: investment,
+          })
+        }
       } else {
-        let payload = investment[0].$original
-        // Date payout was effected due to termination
-        payload.datePayoutWasDone = new Date().toISOString()
-        console.log('Payout investment data 1:', payload)
-        const payout = await Payout.create(payload)
-        payout.status = 'terminated'
-        await payout.save()
-        console.log('Terminated Payout investment data 1:', payout)
-        // investment = await Investment.query().where('id', params.id).where('user_id', id).delete()
-        investment = await Investment.query().where('id', investmentId)
-        investment[0].status = 'terminated'
-        // @ts-ignore
-        investment[0].datePayoutWasDone = new Date().toISOString()
-        investment[0].save()
-        console.log('Terminated Payout investment data 2:', investment)
-        return response.status(200).json({
-          status: 'ok',
-          data: investment,
+        return response.status(404).json({
+          status: 'fail',
+          message: 'no investment matched your search',
+          data: [],
         })
       }
     } catch (error) {
