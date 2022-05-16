d3a6486d0058cd267b1d5d042b28dab0deac012a
Michael Alao <contactleomax@gmail.com>
2022-05-16 16:49:18 +0100

    --- Auto-Git Commit ---

--- app/Controllers/Http/InvestmentsController.ts
+++ app/Controllers/Http/InvestmentsController.ts
@@ -915,33 +915,108 @@ export default class InvestmentsController {
     }
   }
 
-  public async processPayment({ params, request, response }: HttpContextContract) {
+  public async processPayment({ request, response }: HttpContextContract) {
     try {
       // @ts-ignore
       // let id = request.input('userId')
       let { userId, investmentId } = request.all()
       console.log(
-        'Params for update line 644: ',
+        'Params for update line 924: ',
         +'userId ' + userId + 'investmentId ' + investmentId
       )
-      // const investment = await Investment.query().where('user_id', id).where('id', params.id).delete()
-      // let investment = await Investment.query().where('user_id', id).where('id', params.id)
       let investment = await Investment.query().where('id', investmentId)
-      console.log('Investment Info, line 648: ', investment)
+      console.log('Investment Info, line 928: ', investment)
       if (investment.length > 0) {
         console.log('investment search data :', investment[0].$original)
         // @ts-ignore
         // let isDueForPayout = await dueForPayout(investment[0].startDate, investment[0].duration)
         // console.log('Is due for payout status :', isDueForPayout)
 
-        // TESTING
-        let startDate = DateTime.now().minus({ days: 5 }).toISO()
-        let duration = 6
-        console.log('Time investment was started line 809: ', startDate)
-        let isDueForPayout = await dueForPayout(startDate, duration)
-        console.log('Is due for payout status line 812:', isDueForPayout)
 
-        if (isDueForPayout) {
+        // Send Payment Details to Transaction Service
+
+        // Notify
+        let payoutIsApproved = true
+        if (payoutIsApproved) {
+          // Check Rollover Type
+ let rolloverType = investment[0].rolloverType
+let amount = investment[0].amount
+ let rolloverTarget = investment[0].rolloverTarget
+ let rolloverDone = investment[0].rolloverDone
+
+  // Check RollOver Target
+ if (rolloverDone === rolloverTarget) {
+   //  Proceed to payout the Total Amount due on maturity
+   return response.send({ status: 'OK', message: 'Rollover target has been reached.' })
+ } else {
+   const effectRollover = (amount, rolloverType, rolloverTarget, rolloverDone) => {
+     return new Promise((resolve, reject) => {
+       if (!amount || !rolloverType || !rolloverTarget || !rolloverTarget || amount <= 0)
+         reject(new Error('Incomplete parameters or amount is less than allowed range'))
+       let rate
+       if (parseInt(duration) >= 90 && 180 > parseInt(duration)) {
+         duration = '3 months'
+       } else if (parseInt(duration) >= 180 && 270 > parseInt(duration)) {
+         duration = '6 months'
+       } else if (parseInt(duration) >= 270 && 360 > parseInt(duration)) {
+         duration = '9 months'
+       } else if (parseInt(duration) >= 360 && 450 > parseInt(duration)) {
+         duration = '12 months'
+       } else if (parseInt(duration) >= 450 && 540 > parseInt(duration)) {
+         duration = '1 year and 3 months'
+       } else if (parseInt(duration) >= 540 && 630 > parseInt(duration)) {
+         duration = '1 year and 6 months'
+       } else if (parseInt(duration) >= 630 && 720 > parseInt(duration)) {
+         duration = '1 year and 9 months'
+       } else if (parseInt(duration) >= 720) {
+         duration = '2 years or more'
+       }
+
+       switch (duration) {
+         case '3 months':
+           rate = 0.06
+           console.log(`RATE for ${investment_type} deposit for ${duration} days is:`, rate)
+           break
+         case '6 months':
+           rate = 0.07
+           console.log(`RATE for ${investment_type} deposit for ${duration} days is:`, rate)
+           break
+         case '9 months':
+           rate = 0.08
+           console.log(`RATE for ${investment_type} deposit for ${duration} days is:`, rate)
+           break
+         case '12 months':
+           rate = 0.09
+           console.log(`RATE for ${investment_type} deposit for ${duration} days is:`, rate)
+           break
+         case '1 year and 3 months':
+           rate = 0.1
+           console.log(`RATE for ${investment_type} deposit for ${duration} days is:`, rate)
+           break
+         case '1 year and 6 months':
+           rate = 0.11
+           console.log(`RATE for ${investment_type} deposit for ${duration} days is:`, rate)
+           break
+         case '1 year and 9 months':
+           rate = 0.12
+           console.log(`RATE for ${investment_type} deposit for ${duration} days is:`, rate)
+           break
+         case '2 years or more':
+           rate = 0.13
+           console.log(`RATE for ${investment_type} deposit for ${duration} days is:`, rate)
+           break
+         default:
+           rate = 0.05
+           console.log(`RATE for ${investment_type} deposit for ${duration} days is:`, rate)
+           break
+       }
+       return resolve(rate)
+     })
+   }
+  }
+
+
+  
           let payload = investment[0].$original
           // send to Admin for approval
           let userId = payload.userId
@@ -1043,14 +1118,6 @@ export default class InvestmentsController {
     } catch (error) {
       console.error(error)
     }
-      // Check Rollover Type
-
-      // Check RollOver Target
-
-      // Send Payment Details to Transaction Service
-
-      // Notify
-
   }
 
   public async transactionStatus({ params, request, response }: HttpContextContract) {
