9620b98914dc3531d1ee5590c5fd4e97854c95f0
Michael Alao <contactleomax@gmail.com>
2022-05-24 14:10:03 +0100

    --- Auto-Git Commit ---

--- app/Helpers/utils.ts
+++ app/Helpers/utils.ts
@@ -10,8 +10,8 @@
 // import { DateTime } from 'luxon'
 // const { DateTime } = require('luxon')
 // const {DateTime} = Luxon
-// import Env from '@ioc:Adonis/Core/Env'
-const Env = require('@ioc:Adonis/Core/Env')
+import Env from '@ioc:Adonis/Core/Env'
+// const Env = require('@ioc:Adonis/Core/Env')
 const axios = require('axios').default
 const JSJoda = require('js-joda')
 // const LocalDate = JSJoda.LocalDate
