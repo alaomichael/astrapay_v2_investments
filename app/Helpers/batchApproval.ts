f5f111081d2a2942424a5cd5bf66ccd2ce739ba9
Michael Alao <contactleomax@gmail.com>
2022-05-25 16:49:04 +0100

    Heroku

--- app/Helpers/batchApproval.ts
+++ app/Helpers/batchApproval.ts
@@ -1,5 +1,8 @@
-const batchApproval = async (investmentId, walletId, requestType) => {
+const batchApproval = async (investments, investmentId, walletId, requestType) => {
   console.log(investmentId, walletId, requestType)
+  for await (const inv of investments) {
+    console.log('Loop ', inv)
+  }
 }
 
 export { batchApproval }
