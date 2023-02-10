049a5d0086e93c2a4b9f55d5e500efe5123b9fe4
Michael <contactleomax@gmail.com>
2023-02-10 18:21:32 +0100

    --- Auto Git Commit ---

--- app/Validators/CreateSettingValidator.ts
+++ app/Validators/CreateSettingValidator.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import BaseValidator from './BaseValidator'
